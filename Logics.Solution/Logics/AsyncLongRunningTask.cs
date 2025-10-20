2eefbbc738e305eeaf02442f30edac4740ff4070
ccmmasi <ccmmasi@hotmail.com>
2025-10-20 08:28:09 -0500

    first upload

---
+++ Logics.Solution/Logics/AsyncLongRunningTask.cs
@@ -0,0 +1,18 @@
+﻿namespace Logics
+{
+    public class AsyncLongRunningTask
+    {
+        static async Task Main(string[] args)
+        {
+            var id = System.Threading.Thread.CurrentThread.ManagedThreadId;
+            var task = LongRunningTaskAsync();
+            await task;
+        }
+
+        static async Task LongRunningTaskAsync()
+        {
+            var id = System.Threading.Thread.CurrentThread.ManagedThreadId;
+            await Task.Delay(5000);
+        }
+    }
+}
---
+++ Logics.Solution/Logics/ConcurrentLongRunningTasks.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace Logics
+{
+    public class ConcurrentLongRunningTasks
+    {
+        static async Task Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Task task1 = LongRunningOperation("Task 1");
+            Task task2 = LongRunningOperation("Task 2");
+            Task task3 = LongRunningOperation("Task 3");
+            Task task4 = LongRunningOperation("Task 4");
+
+            await Task.WhenAll(task1, task2, task3, task4);
+            stopwatch.Stop();
+
+            Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        static async Task LongRunningOperation(string taskName)
+        {
+            Console.WriteLine($"{taskName} started...");
+            await Task.Delay(5000);
+            Console.WriteLine($"{taskName} completed...");
+        }
+    }
+}
