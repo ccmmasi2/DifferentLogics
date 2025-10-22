cd344fb7c4fbfebf0d11298baf32193392cb4a5f
ccmmasi <ccmmasi@hotmail.com>
2025-10-22 09:20:30 -0500

    otros codigos

---
+++ Logics.Solution/Logics/RabbitMQConsumer.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Logics
+{
+    public class RabbitMQConsumer
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection()) 
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "my_queue",
+                                        durable: false,
+                                        autoDelete: false,
+                                        exclusive: false,
+                                        arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (sender, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Received message: {0}", message);
+                };
+
+                channel.BasicConsume(queue: "my_queue",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine("Waiting for messages...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Logics.Solution/Logics/RabbitMQProducer.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Logics
+{
+    public class RabbitMQProducer
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "my_queue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello, RabbitMQ!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "my_queue",
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine("Message published to the queue.");
+            }
+        }
+    }
+}
